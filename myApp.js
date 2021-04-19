cb10f51b07511235bba5de873381ff6d124694fd
mrPity28 <marvalp.dev@gmail.com>
2021-04-18 23:45:29 -0400

    practica

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+var absolutePath = __dirname + '/views/index.html'
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+    res.sendFile(absolutePath);
+  });
 
 
 
